92197d3096ea88df961273de06f4a9fb1281028b
song07825 <67132433+song07825@users.noreply.github.com>
2020-06-19 15:17:06 +0900

    Update NickList.py

--- NickList.py
+++ NickList.py
@@ -1,4 +1,5 @@
 import discord
+import os
 
 client = discord.Client()
 
@@ -36,5 +37,5 @@ async  def on_message(message):
 
 
 
-
-client.run("Njk4Mzg1OTgzMzI2MTI2MDkw.XurU7g.zSnnMXx6Hz7bJ-vm90wHL4ElG1s")
+access_token = os.environ["BOT_TOKEN"]
+client.run(access_token)
